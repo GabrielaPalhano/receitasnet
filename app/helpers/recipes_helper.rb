d514de0201a8cff23c51474aa4ab02e282f3ab7e
gabi <g.agpalhano@gmail.com>
2019-05-26 22:14:58 -0300

    Aula 16

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+
+    def imagem (recipe)
+        if recipe.poster.blank?
+            image_tag('noimage.png')
+        else
+            image_tag(recipe.poster, :width => 400, :height => 250)
+        end
+    end
+
 end
